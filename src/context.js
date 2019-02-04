f58f7bb55189aff642881ef13b23b910b1c0d9a6
Paul Young <paul@x.net.au>
2019-02-04 18:54:03 +1100

    got fetch working instead of axios

--- src/context.js
+++ src/context.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const Context = React.createContext()
 
@@ -10,12 +10,15 @@ export class Provider extends Component {
 	}
 
 	componentDidMount() {
-		axios
-		.get(
-		  `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
-			process.env.REACT_APP_MM_KEY}`
+		// axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+		// 	.then(res => console.log(res.data))
+		// 	.catch(err => console.log(err))
+
+		fetch(
+			`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
 		)
-			.then(res => console.log(res.data))
+			.then(res => res.json())
+			.then(res => console.log(res))
 			.catch(err => console.log(err))
 	}
 
