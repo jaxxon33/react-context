12812e0fbf2496a85528a9121f51d9724c9058a9
Paul Young <paul@x.net.au>
2019-02-04 12:04:56 +1100

    update

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
 import Index from './components/layouts/Index'
 
-const key = '6e3fa18eda8b5a5ed63360fc1fa92811'
+import { Provider } from './context'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container"></div>
-          <Switch>
-            <Route exact path="/" component={Index}></Route>
-        </Switch>
-        </React.Fragment>
-      </Router>
-
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container"></div>
+            <Switch>
+              <Route exact path="/" component={Index}></Route>
+            </Switch>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
