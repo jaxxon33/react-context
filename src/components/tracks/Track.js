7216258d25a5c9189a8ea8cfe5028205881abceb
Paul Young <paul@MacMini.local>
2019-02-07 15:57:34 +1100

    update

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Track = (props) => {
 	const { track } = props
@@ -8,14 +7,6 @@ const Track = (props) => {
 			<div className="card mb-4 shadow-sm">
 				<div className="card-body">
 					<h5>{track.artist_name}</h5>
-					<p className="card-text">
-						<strong><i className="fas fa-play"></i>Track</strong>: {track.track_name}
-						<br />
-						<strong><i className="fas fa-compact-disc"></i>Album</strong>: {track.album_name}
-					</p>
-					<Link to={`lyrics/track/${track.track_id}`} className='btn btn-dark btn-block'>
-						<i className="fas fa-chevron-right"> View Lyrics</i>
-					</Link>
 				</div>
 			</div>
 		</div>
