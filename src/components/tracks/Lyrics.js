39f4244c1b288cac640deca34223f423f2db433e
Paul Young <paul@MacMini.local>
2019-02-08 06:30:46 +1100

    added lyrics functionality

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Spinner from '../layouts/spinner'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 
 export default class Lyrics extends Component {
 	state = {
 		track: {},
-		lyrics:{}
+		lyrics: {}
 	}
 
 	componentDidMount() {
@@ -12,18 +15,65 @@ export default class Lyrics extends Component {
 		)
 			.then(res => res.json())
 			.then(res => {
-				//this.setState({ track_list: res.message.body.track_list })
+				this.setState({ lyrics: res.message.body.lyrics })
 				console.log(res)
 				//console.log(process.env.REACT_APP_MM_KEY)
 				//console.log(this.props.match.params.id)
+				return fetch(
+					`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+				)
+					.then(res => res.json())
+					.then(res => {
+						this.setState({ track: res.message.body.track })
+						console.log(res)
+					})
+
+
 			})
 			.catch(err => console.log(err))
 	}
 	render() {
-	return (
-	  <div>
-		<h1>Lyrics</h1>
-	  </div>
-	)
-  }
+		const { track, lyrics } = this.state
+		if (
+			track === undefined ||
+			lyrics === undefined ||
+			Object.keys(track).length === 0 ||
+			Object.keys(lyrics).length === 0
+		) {
+			return (
+				<Spinner />
+			)
+
+		} else {
+			return (
+				<React.Fragment>
+					<Link to='/' className='btn btn-dark btn-sm mb-4'>Go Back</Link>
+					<div className="card">
+						<h5 className="card-header">
+							{track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+						</h5>
+						<div className="card-body">
+							<p className="card-text">{lyrics.lyrics_body}
+							</p>
+						</div>
+					</div>
+					<ul className="list-group mt-3">
+						<li className="list-group-item">
+							<strong>Album ID</strong>: {track.album_id}
+						</li>
+						<li className="list-group-item">
+							<strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+						</li>
+						<li className="list-group-item">
+							<strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+						</li>
+						<li className="list-group-item">
+							{/* was meant to be release date but not returned from API - used 'moment' and 'react-moment' */}
+							<strong>Release Date</strong>: <Moment format='DD MM YYYY'>{track.updated_time}</Moment>
+						</li>
+					</ul>
+				</React.Fragment>
+			)
+		}
+	}
 }
